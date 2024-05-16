9060beab143a87a86f679e8c90d6e1b88c2e5784
Hozaneybo <maheybo01@easv365.dk>
2024-05-16 15:19:26 +0200

    ...5

--- frontend/lib/utils/constants.dart
+++ frontend/lib/utils/constants.dart
@@ -1,5 +1,5 @@
 const String WEBSOCKET_URL = 'ws://192.168.0.165:8181';
 const double kMobileWidth = 320.0;
 const double kMobileHeight = 240.0;
-const double kWebWidth = 640.0;
-const double kWebHeight = 480.0;
+const double kWebWidth = 400.0;
+const double kWebHeight = 600.0;
